d0aafbf860321830fb9d2037621e5bcea57dc8f8
rachealkuranchie <kuranchieracheal@gmail.com>
2024-09-18 00:16:33 +0000

    added checkout

--- src/order/controllers.py
+++ src/order/controllers.py
@@ -1,5 +1,8 @@
+import os
+from crypt import methods
 
-from flask import request
+from dotenv import load_dotenv
+from flask import request, jsonify, redirect
 from flask_jwt_extended import jwt_required, get_jwt_identity
 
 from .models import Order, OrderItem
@@ -7,13 +10,16 @@ from marshmallow import Schema, fields,  ValidationError
 from .. import app, create_response, User, db
 from ..carts.controllers import get_total
 from ..products.models import Product
+import requests
+
+load_dotenv()
 
 
 class OrderItemSchema(Schema):
     id = fields.UUID(dump_only=True)
     product_id = fields.UUID(required=True)
     quantity = fields.Integer(required=True )
-    price_at_time = fields.Float(dump_only=True)
+
 
 
 
@@ -69,55 +75,75 @@ def create_order():
             customer_name=data['customer_name'],
             note=data.get('note'),
             has_delivery=data['has_delivery'],
+            delivery_fee = data.get('delivery_fee'),
             country=data['country'],
             region=data['region'],
             city=data.get('city'),
             longitude=data.get('longitude'),
             latitude=data.get('latitude'),
             user_id=user.id,
-            order_number = generate_order_number()
+
         )
 
         db.session.add(new_order)
         print(f' product {new_order}')
-        # db.session.flush()
+        try:
+            db.session.flush()
+            print(f'Flushed to DB, Order ID: {new_order.id}')
+
+        except Exception as e:
+            print(f'Error during flush: {e}')
+            db.session.rollback()
+            return create_response(error=str(e),message="An error occurred while creating the order", status=500)
 
         sub_total = 0
-        print(f' product {sub_total}')
+        print(f' sub {sub_total}')
+
         for item in data['items']:
-            product = Product.query.get(item['product_id'])
+
+            product_id = item['product_id']
+            print(f"ite  {product_id}")
+
+            product = Product.query.filter(Product.id == product_id).first()
+
             print(f' product {product}')
             if not product:
-                db.session.rollback()
+                print(f' product stock {product.stock_quantity}')
+
                 return create_response(error=f"Product with id {item['product_id']} not found", message="Product not found", status=404)
 
             if product.stock_quantity < item['quantity']:
-                print(f' product {product}')
-                db.session.rollback()
+                print(f' product stock {product.stock_quantity}')
+
                 return create_response(error=f"Insufficient stock for product {product.name}", message="Insufficient stock", status=400)
 
             order_item = OrderItem(
                 order_id=new_order.id,
                 product_id=item['product_id'],
                 quantity=item['quantity'],
-                price_at_time=product.price
+
             )
             db.session.add(order_item)
+
             product.stock_quantity -= item['quantity']
 
             sub_total += product.price * item['quantity']
 
-            print(f' sub {sub_total}')
+
 
         new_order.sub_total = sub_total
-        print(f' sub {new_order.sub_total}')
+
 
 
         new_order.total_amount_due = sub_total + new_order.delivery_fee
-        print(f' sub {new_order.total_amount_due}')
+
         db.session.commit()
 
-        return create_response(data={"order_id": str(new_order.id), "order_number": new_order.order_number},
+        response, status = checkout(new_order.id)
+        payment_data = response.json
+
+
+        return create_response(data={"order_id": str(new_order.id), "order_number": new_order.order_number, 'payment': payment_data['data']},
                                message="Order created successfully", status=201)
 
     except Exception as e:
@@ -126,8 +152,108 @@ def create_order():
 
 
 
-def generate_order_number():
-    last_order = Order.query.order_by(Order.id.desc()).first()
-    if last_order:
-        return f"ORD-{int(last_order.order_number.split('-')[1]) + 1:06d}"
-    return "ORD-000001"
+
+
+def checkout(order_id):
+    try:
+        order = Order.query.filter(Order.id == order_id).first()
+        if not order:
+            return jsonify({
+                "error": "Order not found",
+                "message": "The specified order does not exist",
+                "status": 404
+            }), 404
+
+        headers = {
+            "Authorization": f"Bearer {os.getenv('PAYSTACK_SECRET')}"
+
+        }
+        data = {
+            "amount": int(round(order.total_amount_due) * 100),
+            "currency": "GHS",
+            "email": order.customer_email,
+
+
+        }
+
+        try:
+            transaction = requests.post('https://api.paystack.co/transaction/initialize', json=data, headers=headers)
+
+            response = transaction.json()
+            auth = response['data']
+            order.payment_reference = auth['reference']
+
+            return jsonify({
+                "data": auth,
+                "message": "success",
+                "status": 200
+            }), 200
+
+        except requests.RequestException as e:
+            return jsonify({
+                "error": str(e),
+                "message": "Error while trying to make payment",
+                "status": 500
+            }), 500
+
+    except Exception as e:
+        return jsonify({
+            "error": str(e),
+            "message": "An unexpected error occurred",
+            "status": 500
+        }), 500
+
+
+
+@app.route('/paystack/callback', methods=['POST'])
+def callback_payment():
+    try:
+        payload = request.json
+        if not payload or 'event' not in payload:
+            return jsonify({'status': 'failed', 'message': 'Invalid payload'}), 400
+
+        ref = payload['data']['reference']
+
+
+        url = f'https://api.paystack.co/transaction/verify/{ref}'
+        headers = {
+            "Authorization": f"Bearer {os.getenv('PAYSTACK_SECRET')}"
+
+        }
+
+        try:
+            transaction = requests.get(url,  headers=headers)
+
+            response = transaction.json()
+            verification = response['data']
+
+            if verification['data']['status'] == 'success':
+                order = Order.query.filter_by(payment_reference=ref).first()
+
+                if order:
+                    order.payment_status = 'success'
+                    order.amount_paid = verification['data']['amount'] / 100
+                    db.session.commit()
+
+                    return jsonify({'status': 'success', 'message': 'Payment verified and order updated'}), 200
+                else:
+                    return jsonify({'status': 'failed', 'message': 'Order not found'}), 404
+            else:
+                return jsonify({'status': 'failed', 'message': 'Payment verification failed'}), 400
+
+
+
+        except Exception as e:
+            db.session.rollback()
+            return jsonify({'status': 'failed', 'message': 'An unexpected error occurred'}), 500
+
+
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({'status': 'failed', 'message': 'An unexpected error occurred'}), 500
+
+
+
+
+
+
